b9a2b54f5f9a87216288dba562ddeb2b33bd6bb9
stanpov <popovstanislav@abv.bg>
2023-08-29 22:57:12 -0700

    Fix logout api call

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ mongoose
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
