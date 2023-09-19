fa0bbeface5e411fe20dbd764ce397863266d65a
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-09-19 12:17:03 +0300

    CSearchInput template created

---
+++ client/src/components/common/CSearchInput/CSearchInput.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CSearchInput.scss';
+import { BsSearch } from 'react-icons/bs';
+
+interface CSearchInputProps { }
+
+export const CSearchInput: React.FC<CSearchInputProps> = () => {
+    return (
+        <form className='search'>
+            <label htmlFor="search" className='search__input'>
+                <BsSearch />
+                <input type="text" id='search' name='search' />
+            </label>
+
+            <input className='search__submit' type="submit" id='submit' name='submit' value={'Search'} />
+
+
+        </form>
+
+    )
+}
--- client/src/pages/Products/Products.tsx
+++ client/src/pages/Products/Products.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { PageWrapper } from "../../components/common/PageWrapper/PageWrapper";
 import { CRadio } from "../../components/common/CRadio/CRadio";
 import './Products.scss';
+import { FaList } from "react-icons/fa";
+import { BsFillGridFill } from "react-icons/bs";
+import { CSearchInput } from "../../components/common/CSearchInput/CSearchInput";
 // import { CInputSubmit } from "../../components/common/CInputSubmit/CInputSubmit";
 
 interface ProductsProps {
@@ -9,7 +12,7 @@ interface ProductsProps {
 }
 
 const category = ["TV", "Phones", "Laptops", "Headphones", "Monitors"];
-const brands = ['Sony','Asus','Philips','Huawei','Apple'];
+const brands = ['Sony', 'Asus', 'Philips', 'Huawei', 'Apple'];
 
 const Products: React.FC<ProductsProps> = () => {
     return (
@@ -19,7 +22,7 @@ const Products: React.FC<ProductsProps> = () => {
                     <form className="products__filter__form">
                         <section className="products__filter__form__section">
                             <h4>category</h4>
-                            <CRadio category="category" radioName="All" />
+                            <CRadio category="category" radioName="All categories" />
                             {category.map(x => <CRadio category="category" radioName={x} key={x} />)}
                         </section>
                         <section className="products__filter__form__section">
@@ -37,7 +40,16 @@ const Products: React.FC<ProductsProps> = () => {
                 </aside>
                 <section className="products__section">
                     <article className="products__section__sorter">
+                        <div className="products__section__sorter__icons">
+                            <BsFillGridFill />
+                            <FaList />
+                        </div>
+                        <div className="products__section__sorter__search">
+                            <CSearchInput />
+                        </div>
+                        <div className="products__section__sorter__sort">
 
+                        </div>
                     </article>
                     <article className="products__section__grid">
 
