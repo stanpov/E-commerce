2a716199d5ae2106bed4cd358bd215d2b629ab90
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-09-27 16:30:42 +0300

    ProductDetails responsive design fixed

--- client/src/components/common/MotionDownCorner/MotionDownCorner.tsx
+++ client/src/components/common/MotionDownCorner/MotionDownCorner.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import './MotionDownCorner.scss';
 export const MotionDownCorner = () => {
@@ -10,6 +9,7 @@ export const MotionDownCorner = () => {
             }}
             initial='start'
             whileInView='end'
+            viewport={{ once: true }}
             transition={{ duration: 1, delay: 0.2 }}
             className='corner__gradient__down'>
         </motion.div>
